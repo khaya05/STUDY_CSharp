49fce019ec3cae84681dabbbc0a85c75c7833fef
khaya05 <khayamnyandu5@gmail.com>
2023-04-07 11:20:05 +0200

    added logic

--- src/ExistsExample/Program.cs
+++ src/ExistsExample/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExistsExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // marks collection 
+            List<int> marks = new List<int>() { 40, 45, 74, 91, 50, 81 };
+
+            // check if student failed
+            bool failed = marks.Exists(mark => mark <= 35);
+
+
+            Console.WriteLine(failed ? "FAILED" : "PASSED");
+        }
+    }
+}
