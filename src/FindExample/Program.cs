91a4442e91f3ddec8dcb34bd5dc667971953abd1
khaya05 <khayamnyandu5@gmail.com>
2023-04-07 11:28:32 +0200

    Find() Example

---
+++ src/FindExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // marks collection 
+            List<int> marks = new List<int>() { 40, 45, 24, 91, 50, 81 };
+
+            // get marks of failed subject
+            int grade = marks.Find(x => x < 35);
+            int index = marks.IndexOf(grade);
+
+
+            Console.WriteLine(index);
+        }
+    }
+}
