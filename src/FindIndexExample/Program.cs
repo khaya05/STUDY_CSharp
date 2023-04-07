05d7d6eb589a13995dcec6cca74b99b6ed9dc382
khaya05 <khayamnyandu5@gmail.com>
2023-04-07 11:35:00 +0200

    findIndex Example

---
+++ src/FindIndexExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindIndexExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // marks collection 
+            List<int> marks = new List<int>() { 40, 45, 24, 91, 50, 81 };
+
+            // Get index of marks first failed
+            int index = marks.FindIndex(x => x < 35);
+
+            Console.WriteLine(index);
+        }
+    }
+}
