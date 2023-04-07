ec81986c7ec017d7ad61f42013516649ad7cae5e
khaya05 <khayamnyandu5@gmail.com>
2023-04-07 11:46:37 +0200

    FindAll() Example

---
+++ src/FindAll Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindAllExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // marks collection 
+            List<int> marks = new List<int>() { 40, 45, 24, 91, 30, 81 };
+
+            // Get index of marks of failed subjects
+            List<int> failedSubjects = marks.FindAll(x => x < 35);
+
+            // print collection
+            Console.WriteLine("\nFailed Marks:");
+            foreach (int item  in failedSubjects)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
