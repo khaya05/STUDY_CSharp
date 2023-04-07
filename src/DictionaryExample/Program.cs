d3c024c045045225f3d4e011662aafa0944dc435
khaya05 <khayamnyandu5@gmail.com>
2023-04-07 13:01:32 +0200

    Dictionary Example

--- src/DictionaryExample/Program.cs
+++ src/DictionaryExample/Program.cs
@@ -18,6 +18,27 @@ namespace DictionaryExample
       {
         Console.WriteLine(item.Key + ", " + item.Value);
       }
+
+      string s = employees[101];
+      Console.WriteLine("Value at 101: " + s);
+
+      // access keys
+      Dictionary<int, string>.KeyCollection keys = employees.Keys;
+
+      Console.WriteLine("Dict Keys:-");
+      
+      foreach(int key in keys)
+      {
+        Console.WriteLine(key);
+      }
+
+      // ContainsKey
+      bool a = employees.ContainsKey(104);
+      Console.WriteLine("ContainsKey 104:- " + a);
+
+      // ContainsValue 
+      bool b = employees.ContainsValue("Larry");
+      Console.WriteLine("ContainsValue Larry:- " + b);
     }
   }
 }
