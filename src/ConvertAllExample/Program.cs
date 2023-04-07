cb8f8ea7259be758c90982b09c0dcce417fe22b5
khaya05 <khayamnyandu5@gmail.com>
2023-04-07 12:13:50 +0200

    ConvertAll Example

---
+++ src/ConvertAllExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConvetAllExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> intList = new List<int>() { 1, 3, 5, 8, 9 };
+            List<string> strCollection = intList.ConvertAll<string>(n =>
+            {
+                // squre n
+                int m = n * n;
+                return Convert.ToString(m);
+            });
+
+            // print the result
+            foreach(string str in strCollection)
+            {
+                Console.WriteLine(str);
+            }
+
+        }
+
+    }
+}
