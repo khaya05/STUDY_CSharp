0b51491a2eca936de4e36e1fb6f7f88779f1a419
khaya05 <khayamnyandu5@gmail.com>
2023-04-07 12:36:57 +0200

    Dictionary() Example

---
+++ src/DictionaryExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryExample
+{
+  class Program
+  {
+    static void Main()
+    {
+      Dictionary<int, string> employees = new Dictionary<int, string>()
+      {
+        {101, "Larry"},
+        {102, "Page"},
+        {103, "Nancy"}
+      };
+
+      foreach(KeyValuePair<int, string> item in employees)
+      {
+        Console.WriteLine(item.Key + ", " + item.Value);
+      }
+    }
+  }
+}
