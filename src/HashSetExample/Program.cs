43cda22ffc3428de33247f9ec3b4b9d614d54bed
khaya05 <khayamnyandu5@gmail.com>
2023-04-17 07:30:56 +0200

    Example

--- src/HashSetExample/Program.cs
+++ src/HashSetExample/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetExample
+{
+  class Program
+  {
+    static void Main()
+    {
+      // create a hash set
+      HashSet<string> fruits = new HashSet<string>()
+      {
+        "Apple", "Banana", "Litchi"
+      };
+
+      // add
+      fruits.Add("Oranges");
+
+      // removeWhere
+      fruits.RemoveWhere(x => x.EndsWith('i'));
+
+      Console.WriteLine("Count: " + fruits.Count);
+      // loop
+      foreach (string fruit in fruits)
+      {
+        Console.WriteLine(fruit);
+      }
+
+    }
+  }
+}
