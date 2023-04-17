5f4c5fe46a4d2ff3f863d2833148791171bd0b98
khaya05 <khayamnyandu5@gmail.com>
2023-04-17 14:08:09 +0200

    CollectionOfObjects Example

---
+++ src/CollectionOfObjectsClassLib/Ecommerce.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ecommerce
+{
+  /// <summary>
+  /// Represents a product in e-commerce application;
+  /// </summary>
+  public class Product
+  {
+    // properties
+    public int Id{ get; set; }
+    public string Name{ get; set; }
+    public double Price{ get; set; }
+    public DateTime DateOfManufacture{ get; set; }
+
+  }
+
+}
