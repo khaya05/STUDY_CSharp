dcd5bf70e40e5a208827b82d2754350af250a91a
khaya05 <khayamnyandu5@gmail.com>
2023-04-17 11:25:16 +0200

    ArrayList Example

---
+++ src/ArrayListExample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListExample
+{
+  class Sample
+  {
+    public int GetNumber()
+    {
+      return 28;
+    }
+
+    public double GetDouble()
+    {
+      return 10.6;
+    }
+
+    public string GetMessage()
+    {
+      return "Hello";
+    }
+
+    public Employee GetEmployee()
+    {
+      return new Employee(){EmployeeName = "Scott"};
+    }
+  }
+
+  class Employee
+  {
+    public string EmployeeName{ get; set; }
+  }
+
+  class Program
+  {
+    static void Main()
+    {
+      Sample s = new Sample();
+      // create object
+      ArrayList arrayList = new ArrayList() { 100, 'A' };
+
+      // Add
+      arrayList.Add(s.GetNumber());
+      arrayList.Add(s.GetDouble());
+      arrayList.Add(s.GetMessage());
+      arrayList.Add(s.GetEmployee());
+
+      //print
+
+      foreach (var item in arrayList)
+      {
+        if(item is Employee emp)
+        {
+          Console.WriteLine(emp.EmployeeName);
+        }
+        else
+        {
+          Console.WriteLine(item);
+        }
+      }
+    }
+  }
+}
