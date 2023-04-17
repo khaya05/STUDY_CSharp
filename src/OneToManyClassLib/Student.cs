180978c897cdf3bd31b331015b80b2efec3c0981
khaya05 <khayamnyandu5@gmail.com>
2023-04-17 15:36:58 +0200

    Relations Example

---
+++ src/OneToManyClassLib/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace College
+{
+    /// <summary>
+    /// Represents a student
+    /// </summary>
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public String Email { get; set; }   
+        public List<Examination> examinations { get; set; }
+    }
+}
+ 
---
+++ src/OneToManyExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using College;
+
+namespace OneToManyExample
+{
+    class Progam
+    {
+        public static void Main()
+        {
+            // obj of student Class
+            Student student = new Student() { Id = 1, Name = "Allen", Email = "allen@gmail.com"};
+            student.examinations = new List<Examination>();
+
+            student.examinations.Add(new Examination() { ExaminationName = "Test 1", Month = 1 , Year = 2020, MaxMarks = 100, SecuredMarks = 87});
+            student.examinations.Add(new Examination() { ExaminationName = "Test 2", Month = 2, Year = 2020, MaxMarks = 100, SecuredMarks = 77 });
+            student.examinations.Add(new Examination() { ExaminationName = "Test 3", Month = 3, Year = 2020, MaxMarks = 100, SecuredMarks = 55 });
+
+            // print
+            Console.WriteLine();
+        }
+    }
+}
