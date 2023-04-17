90f679c19830789c208e8ddf7c79f5b1799255e0
khaya05 <khayamnyandu5@gmail.com>
2023-04-17 07:51:45 +0200

    UnionWith and IntersectsWith Example

---
+++ src/HashSetExample2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetExample2
+{
+  class Program
+  {
+    static void Main()
+    {
+      HashSet<string> employees2021 = new HashSet<string>() {"Amar", "Akhil", "Prioshia" };
+      HashSet<string> newEmployees2022 = new HashSet<string>() {"John", "Luke", "Zacky" };
+
+      // unionWith
+      employees2021.UnionWith(newEmployees2022);
+
+      // print
+      foreach(string employee in employees2021)
+      {
+        Console.WriteLine(employee);
+      }
+    }
+  }
+}
---
+++ src/HashSetExample3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetExample3
+{
+  class Program
+  {
+    static void Main()
+    {
+      HashSet<string> employees2021 = new HashSet<string>() {"Amar", "Akhil", "Prioshia" };
+      HashSet<string> employees2022 = new HashSet<string>() {"Amar", "Akhil","John", "Luke", "Zacky" };
+
+      // unionWith
+      employees2021.IntersectWith(employees2022);
+
+      // print
+      foreach(string employee in employees2021)
+      {
+        Console.WriteLine(employee);
+      }
+    }
+  }
+}
