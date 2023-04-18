f41ae37cf56920e93e823b47493371539734c897
khaya05 <khayamnyandu5@gmail.com>
2023-04-18 05:48:16 +0200

    IEnumerable Example

---
+++ src/IEnumerableExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerableExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            // collection
+            IEnumerable<string> fruits;
+            fruits = new List<string>() { "Apple", "Mango", "Grape"};
+
+            // foreach
+            Console.WriteLine("IENumerable");
+            foreach (string item in fruits)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
