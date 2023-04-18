2a58a856abc0accad5efa6041703ccdb2293648c
khaya05 <khayamnyandu5@gmail.com>
2023-04-18 08:02:07 +0200

    custom collections

--- src/CustomCollections/Program.cs
+++ src/CustomCollections/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace CustomCollections
+{
+  public enum TypeOfCustomer
+  {
+    RegularCustomer, VIPCustomer
+  }
+
+  public class Customer
+  {
+    public string CustomerID{ get; set; }
+    public string CustomerName{ get; set; }
+    public string Email{ get; set; }
+    public TypeOfCustomer CustomerType{ get; set; }
+  }
+
+  // custom collection class
+  public class CustomerList
+  {
+    
+  }
+}
