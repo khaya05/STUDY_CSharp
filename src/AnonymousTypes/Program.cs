7b9461dbb52737b332adeea8d9cac25171a2f2c7
khaya05 <khayamnyandu5@gmail.com>
2023-04-18 21:59:12 +0200

    AnonymousTypes

--- src/AnonymousTypes/Program.cs
+++ src/AnonymousTypes/Program.cs
@@ -3,4 +3,28 @@
 Person p = new Person();
 
 // call methods
-var person = new { PersonName = p.GetPersonName(), PersonAge = p.GetPersonAge() };
+var person = new { 
+  PersonName = p.GetPersonName(), 
+  PersonAge = p.GetPersonAge(), 
+  Address = new 
+    { 
+      Street = "ADC", 
+      City = "XCV" 
+    } 
+};
+
+
+// anonymous arrays
+
+var newPerson = new[]
+{
+  new {PersonName = "Zack", Email = "Zack@email.com"},
+  new {PersonName = "Luke", Email = "Luke@email.com"},
+  new {PersonName = "Stephen", Email = "Stephen@email.com"}
+};
+
+// foreach
+foreach(var item in newPerson)
+{
+  Console.WriteLine("{0}, {1}", item.PersonName, item.Email);
+}
