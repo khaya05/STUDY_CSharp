3df0e83c2c6f658adebf8425fd042949d3fd7022
khaya05 <khayamnyandu5@gmail.com>
2023-04-22 13:22:46 +0200

    DateTime Format example?

--- src/DateTimeFormats/Program.cs
+++ src/DateTimeFormats/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DateTimeFormats;
+class Program 
+{
+  static void Main()
+  {
+    // DateTime dt = DateTime.Parse("2020.12.31 11:59:59");
+    DateTime dt = DateTime.ParseExact
+    (
+      "31/12/2022 23:59:59", 
+      "dd/MM/yyyy HH:mm:ss", 
+      System.Globalization.CultureInfo.InvariantCulture, 
+      System.Globalization.DateTimeStyles.None
+    );
+
+    string str1 = dt.ToString();
+    Console.WriteLine(dt.ToShortDateString()); 
+    Console.WriteLine(dt.ToLongDateString()); 
+    Console.WriteLine(dt.ToShortTimeString()); 
+    Console.WriteLine(dt.ToLongTimeString()); 
+
+    Console.WriteLine(dt.ToString("dd.MM.yyyy HH:mm:ss")); 
+    // Console.WriteLine(str1); 
+  }
+}
