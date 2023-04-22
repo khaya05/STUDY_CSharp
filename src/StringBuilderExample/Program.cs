f99bba1304b6ff7bc8430f7dcd60084cdb287be2
khaya05 <khayamnyandu5@gmail.com>
2023-04-22 09:43:16 +0200

    basic string builder

---
+++ src/StringBuilderExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace StringBuilderExample;
+class Program 
+{
+  static void Main()
+  {
+    string[] words = new string[] { "The", "lazy", "dog", "did", "the", "thing" };
+    //string builder
+    StringBuilder builder = new StringBuilder();
+    foreach (string word in words)
+    {
+      builder.Append(word);
+      builder.Append(" ");
+    }
+
+    Console.WriteLine(builder.ToString());
+  }
+}
