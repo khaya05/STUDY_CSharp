5b7de6b87510237a61b173b4e95a7a1eac59a397
khaya05 <khayamnyandu5@gmail.com>
2023-04-22 14:01:13 +0200

    DateSubtraction

---
+++ src/DateAdditionAndSubtraction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DateOperations;
+class Employee
+{
+  public string Name { get; set; } = string.Empty;
+  public DateTime DateOfJoining{ get; set; }
+  public double ExperienceYears { get; set; }
+  public double ExperienceMonths { get; set; }
+}
+
+class Program 
+{
+
+  static void Main()
+  {
+    Employee emp = new Employee() 
+    { 
+      Name = "Zack", 
+      DateOfJoining = DateTime.Parse("1999/10/11") 
+    };
+
+    DateTime today = DateTime.Now;
+    if(today.CompareTo(emp.DateOfJoining) == 1)
+    {
+      TimeSpan ts = today.Subtract(emp.DateOfJoining);
+      Console.WriteLine(ts);
+
+    } 
+    else 
+    {
+      Console.WriteLine("Error");
+    };
+  }
+}
