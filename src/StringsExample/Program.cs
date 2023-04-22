fc9d40163931e735154e24c5c74fcb944d6c7bfe
khaya05 <khayamnyandu5@gmail.com>
2023-04-22 07:40:53 +0200

    basic string operations

---
+++ src/StringsExample/Program.cs
@@ -0,0 +1,18 @@
+﻿string name = "developer@example.com";
+char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+// int count = 0;
+
+// for (int i = 0; i < name.Length; i++)
+// {
+//   char currChar = name[i];
+//   if(vowels.Contains(currChar))
+//   {
+//     count++;
+//   }
+// }
+
+// using Linq
+
+Console.WriteLine(name.Count(ch => vowels.Contains(ch)));
+
+// Console.WriteLine(count);
