e541813b475b234395ba00b3ad5c33392da5e291
khaya05 <khayamnyandu5@gmail.com>
2023-04-22 14:37:16 +0200

    Regex Example

---
+++ src/RegexExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace RegexExample;
+class Program 
+{
+  static void Main()
+  {
+    Regex regex = new Regex("[0-9a-zA-Z]");
+    Console.WriteLine("Enter a Digit");
+    string inputValue = Console.ReadLine()!;
+    bool result = regex.IsMatch(inputValue);
+    Console.WriteLine(result);
+  }
+}
